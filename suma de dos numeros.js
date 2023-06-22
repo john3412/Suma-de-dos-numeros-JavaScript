678a6e2c357ff472f3f323483bfbfd058bdb09e1
Jonathan López Ayala <112882084+john3412@users.noreply.github.com>
2023-06-22 17:59:03 -0600

    Add files via upload

---
+++ suma de dos numeros.js
@@ -0,0 +1,10 @@
+function suma (n1, n2, n3) {
+	const resultado = n1 + n2 + n3;
+	console.log(`El resultado es: ${resultado}`);
+}
+
+const n1 = 3;
+const n2 = 4;
+const n3 = 8;
+
+suma(n1, n2, n3);
