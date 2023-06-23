af7bb6b417be5f56d5e68f0ff3deba518d14619a
Jonathan López Ayala <112882084+john3412@users.noreply.github.com>
2023-06-22 18:04:40 -0600

    Update suma de dos numeros.js

--- suma de dos numeros.js
+++ suma de dos numeros.js
@@ -1,10 +1,10 @@
-function suma (n1, n2, n3) {
+function suma (n1, n2, n3) { //Función que contiene los argumentos que se tomaran en cuenta
 	const resultado = n1 + n2 + n3;
-	console.log(`El resultado es: ${resultado}`);
+	console.log(`El resultado es: ${resultado}`); // Muestra el resultado en la consola
 }
 
 const n1 = 3;
 const n2 = 4;
-const n3 = 8;
+const n3 = 8; //Valores definidos con const ya que este tipo de variable no admite undefined
 
-suma(n1, n2, n3);
+suma(n1, n2, n3); // Llamada a la función
